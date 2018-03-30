6f5fded09ac0164b7acc040e074f73dc06a62cf1
Adam Heins <mail@adamheins.com>
2018-03-30 13:16:00 -0400

    Comment and clean up code.

--- ligatures/ligatures.py
+++ ligatures/ligatures.py
@@ -4,7 +4,8 @@ import regex as re
 
 
 # NOTE: we want longer ligatures first
-COMMON_LIGATURE_BASES = sorted(['fi', 'fl', 'i', 'j', 'l', 'f'], key=len, reverse=True)
+COMMON_LIGATURE_BASES = sorted(['fi', 'fl', 'i', 'j', 'l', 'f'], key=len,
+                               reverse=True)
 COMMON_LIGATURES = ['f' + s for s in COMMON_LIGATURE_BASES]
 
 OTHER_LIGATURE_BASES = ['a', 'e', 'o', 'r', 's', 't', 'b', 'h', 'u', 'y', '.',
@@ -16,7 +17,8 @@ SS2LIG_FILE_NAME = 'ss2lig.txt'
 LIG_WORDS_FILE_NAME = 'lig_words.txt'
 
 
-def save_ss2lig_map(ss2lig, fname):
+def _save_ss2lig_map(ss2lig, fname):
+    ''' Save the ss2lig map to a file. '''
     with open(fname, 'w') as f:
         for k, v in ss2lig.items():
             before = ','.join(v['before'])
@@ -24,13 +26,15 @@ def save_ss2lig_map(ss2lig, fname):
             f.write('{}:{};{}\n'.format(k, before, after))
 
 
-def save_lig_words(lig_words, ligatures, fname):
+def _save_lig_words(lig_words, ligatures, fname):
+    ''' Save the list of ligatured-words to a file. '''
     with open(fname, 'w') as f:
         f.write(' '.join(ligatures) + '\n')
         f.write('\n'.join(lig_words))
 
 
-def load_lig_words(fname):
+def _load_lig_words(fname):
+    ''' Load a list of ligatured-words from a file. '''
     with open(fname) as f:
         lines = f.read().splitlines()
     ligatures = lines[0].split(' ')
@@ -38,7 +42,8 @@ def load_lig_words(fname):
     return lig_words, ligatures
 
 
-def load_ss2lig_map(fname):
+def _load_ss2lig_map(fname):
+    ''' Load the ss2lig map from a file. '''
     with open(fname) as f:
         lines = f.readlines()
 
@@ -53,7 +58,7 @@ def load_ss2lig_map(fname):
     return ss2lig
 
 
-def remove_ligs(parts, lig_regex):
+def _remove_ligs(parts, lig_regex):
     ''' Remove and split all parts on any ligatures they may still contain. '''
     parts_no_ligs = []
     for part in parts:
@@ -61,7 +66,9 @@ def remove_ligs(parts, lig_regex):
     return parts_no_ligs
 
 
-def find_words_with_ligatures(words, lig_regex):
+def _find_words_with_ligatures(words, lig_regex):
+    ''' Returns a set of all words matching the ligature regex from the
+        provided list. '''
     words_with_ligatures = set()
     for word in words:
         if lig_regex.search(word):
@@ -69,7 +76,9 @@ def find_words_with_ligatures(words, lig_regex):
     return words_with_ligatures
 
 
-def build_ss2lig_map(words_with_ligatures, lig_regex):
+def _build_ss2lig_map(words_with_ligatures, lig_regex):
+    ''' Build the mapping from substrings to ligatures that can come before and
+        after them. '''
     ss2lig = {}
 
     for word in words_with_ligatures:
@@ -90,31 +99,38 @@ def build_ss2lig_map(words_with_ligatures, lig_regex):
     return ss2lig
 
 
-def replace_successful_matches(text, matches):
+def _replace_successful_matches(text, matches):
+    ''' Replace the unknown ligatures in the text with their matches and return
+        the new corrected text. '''
     successful_matches = list(filter(lambda m: m.success, matches))
     new_text = []
     last_end = 0
 
-    for m in successful_matches:
-        new_text.append(text[last_end:m.start])
-        new_text.append(m.replacement)
-        last_end = m.end
+    for match in successful_matches:
+        new_text.append(text[last_end:match.start])
+        new_text.append(match.replacement)
+        last_end = match.end
     new_text.append(text[successful_matches[-1].end:])
 
     return ''.join(new_text)
 
 
-def compile_lig_regex(ligatures):
+def _compile_lig_regex(ligatures):
+    ''' Compile the list of ligatures into a single regex. '''
     return re.compile('|'.join(sorted(ligatures, key=len, reverse=True)))
 
 
 class LigatureMatch(object):
+    ''' Contains information about possible match candidates for a single
+        unknown ligature. '''
     def __init__(self, original, candidates, start, end):
         self.original = original
         self.candidates = candidates
         self.start = start
         self.end = end
 
+        # The match is considered successful is there is only one possible
+        # candidate.
         if len(candidates) == 1:
             self.success = True
             self.replacement = candidates[0]
@@ -124,14 +140,16 @@ class LigatureMatch(object):
 
 
 class LigatureMap(object):
+    ''' Matches ligatures to where they are missing in text. '''
     def __init__(self, lig_words, ss2lig, ligatures):
         self.lig_words = lig_words
         self.ss2lig = ss2lig
 
         self.ligatures = ligatures
-        self.regex = compile_lig_regex(ligatures)
+        self.regex = _compile_lig_regex(ligatures)
 
     def save(self, datadir):
+        ''' Save the mapping to a directory. '''
         # Create the data directory if it does not yet exist.
         if not os.path.isdir(datadir):
             os.mkdir(datadir)
@@ -139,13 +157,15 @@ class LigatureMap(object):
         ss2lig_path = os.path.join(datadir, SS2LIG_FILE_NAME)
         lig_words_path = os.path.join(datadir, LIG_WORDS_FILE_NAME)
 
-        save_lig_words(self.lig_words, self.ligatures, lig_words_path)
-        save_ss2lig_map(self.ss2lig, ss2lig_path)
+        _save_lig_words(self.lig_words, self.ligatures, lig_words_path)
+        _save_ss2lig_map(self.ss2lig, ss2lig_path)
 
     def query_word(self, parts):
+        ''' Attempt to replace missing ligatures in a word based on
+            non-ligature substring parts. '''
         # Split words on any remaining ligatures, since these won't appear in
         # the ss2lig map.
-        parts = remove_ligs(parts, self.regex)
+        parts = _remove_ligs(parts, self.regex)
 
         # Determine ligatures that neighbouring substrings have in common.
         candidate_ligs = []
@@ -186,6 +206,7 @@ class LigatureMap(object):
         return candidate_words
 
     def query_text(self, text, lig_identifier, verbose=False):
+        ''' Replace missing ligatures in an arbitrarily long string of text. '''
         # Regex to find words that contain one or more occurences of the given
         # ligature identifier.
         lig_word_regex = u'\s(([a-zA-Z]*' + lig_identifier + ')+[a-zA-Z]*)\s'
@@ -225,19 +246,20 @@ class LigatureMap(object):
             print('Failed: {}'.format(num_failed))
             print('Ambiguous: {}'.format(num_ambiguous))
 
-        new_text = replace_successful_matches(text, matches)
+        new_text = _replace_successful_matches(text, matches)
 
         return matches, new_text
 
     def split(self, string):
+        ''' Convenience function to split strings on ligatures in this map. '''
         return self.regex.split(string)
 
 
 def build(words, ligatures=COMMON_LIGATURES):
     ''' Build ligature database. '''
-    lig_regex = compile_lig_regex(ligatures)
-    lig_words = find_words_with_ligatures(words, lig_regex)
-    ss2lig = build_ss2lig_map(lig_words, lig_regex)
+    lig_regex = _compile_lig_regex(ligatures)
+    lig_words = _find_words_with_ligatures(words, lig_regex)
+    ss2lig = _build_ss2lig_map(lig_words, lig_regex)
     return LigatureMap(lig_words, ss2lig, ligatures)
 
 
@@ -246,7 +268,7 @@ def load(datadir):
     ss2lig_path = os.path.join(datadir, SS2LIG_FILE_NAME)
     lig_words_path = os.path.join(datadir, LIG_WORDS_FILE_NAME)
 
-    lig_words, ligatures = load_lig_words(lig_words_path)
-    ss2lig = load_ss2lig_map(ss2lig_path)
+    lig_words, ligatures = _load_lig_words(lig_words_path)
+    ss2lig = _load_ss2lig_map(ss2lig_path)
 
     return LigatureMap(lig_words, ss2lig, ligatures)
